4dcf10b8c409ef71eacbca74a5c7494be46a4b7a
Lovepreet <lovepreetkaul23@gmail.com>
2021-10-21 16:08:24 +0530

    Initial Commit

---
+++ spec/redis/clone_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Redis::Clone do
+  it "has a version number" do
+    expect(Redis::Clone::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
