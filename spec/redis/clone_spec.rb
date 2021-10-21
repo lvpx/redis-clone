8c1193ac401929d535fa005ff793d42eb4d4eb50
Lovepreet <lovepreetkaul23@gmail.com>
2021-10-21 18:48:57 +0530

    Fix gemspec

--- spec/redis/clone_spec.rb
+++ spec/redis/clone_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Redis::Clone do
   it "does something useful" do
     expect(false).to eq(true)
   end
+
+  it "responds to ping" do
+    SERVER_PORT = ENV["SERVER_PORT"]
+    r = Redis::Clone.Server.new(port: SERVER_PORT)
+    expect(r.ping).eq("PONG")
+  end
 end
